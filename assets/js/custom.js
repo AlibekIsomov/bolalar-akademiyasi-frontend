463fcd213af8adc60d578a6d885e791ce303095b
Alibek Isomov <99270031+AlibekIsomov@users.noreply.github.com>
2024-09-27 20:13:14 +0500

    changes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -229,3 +229,52 @@ document.addEventListener('DOMContentLoaded', function() {
 		});
 	});
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+	var swiper = new Swiper('.swiper-container', {
+		slidesPerView: 3,
+		spaceBetween: 30,
+		loop: true, // Enable looping
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true,
+		},
+		breakpoints: {
+			1024: {
+				slidesPerView: 3,
+				spaceBetween: 30,
+			},
+			768: {
+				slidesPerView: 2,
+				spaceBetween: 20,
+			},
+			640: {
+				slidesPerView: 1,
+				spaceBetween: 10,
+			},
+			320: {
+				slidesPerView: 1,
+				spaceBetween: 10,
+			}
+		}
+	});
+});
+
+
+function openModal() {
+    document.getElementById("registrationModal").style.display = "block";
+  }
+
+  document.querySelector(".close-button").onclick = function() {
+    document.getElementById("registrationModal").style.display = "none";
+  }
+
+  window.onclick = function(event) {
+    if (event.target == document.getElementById("registrationModal")) {
+      document.getElementById("registrationModal").style.display = "none";
+    }
+  }
